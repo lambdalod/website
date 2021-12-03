36352aa4d086c503e7d1fc9a61e0b0f9e9293444
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2021-12-03 03:08:28 +0300

    assets and login page forked from pages

---
+++ login/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>lambda_login</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+</head>
+
+<body>
+    <div id="login" class="container d-flex align-items-center justify-content-center" style="width: 100vw;height: 100vh;">
+        <div class="col-xl-5 paper d-grid gap-3">
+            <div>
+                <p class="text-center subheader">Добро пожаловать!</p>
+            </div>
+            <div class="text-center"><input class='line-input' type="text" placeholder="Номер телефона"></div>
+            <div class="text-center"><input class='line-input' type="password" placeholder="Пароль"></div>
+            <div class="d-flex justify-content-center" style="width: 250px;">
+                <div class="form-check"><input class="form-check-input input-checkbox shadow-none text-center" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Чужой компьютер</label></div>
+            </div>
+            <div class="text-center"><button class="btn input-button shadow-none" type="button">Войти</button></div>
+            <div class="text-center"><a class='input-link' href="#">Не помню пароль</a></div>
+        </div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
